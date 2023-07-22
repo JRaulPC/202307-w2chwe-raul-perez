4111a66a71c490283197b209bb912e80d4efb5f9
Raúl Pérez <jrpcassone@gmail.com>
2023-07-23 00:38:23 +0200

    Correct syntax and and naming in cell and gameboard js

--- app/JS/Game/Cell/Cell.js
+++ app/JS/Game/Cell/Cell.js
@@ -1,12 +1,13 @@
 class Cell {
-  isAlive = Math.random() < 0.25;
+  static isAlive;
+
+  constructor() {
+    this.isAlive = Math.random() < 0.25;
+  }
 
   live() {
     this.isAlive = true;
   }
 }
-
-const newCell = new Cell();
-console.log(newCell);
 
 export default Cell;
--- app/JS/Game/generate-game-grid.js
+++ app/JS/Game/generate-game-grid.js
@@ -2,7 +2,7 @@ import Cell from "./Cell/Cell.js";
 
 class GameBoard {
   generateGameBoard(row, rowsLength) {
-    const cellRow = [];
+    const matrix = [];
 
     for (let i = 0; i < row; i++) {
       const row = [];
@@ -11,15 +11,16 @@ class GameBoard {
         row.push(cell);
       }
 
-      cellRow.push(row);
+      matrix.push(row);
     }
 
-    return cellRow;
+    return matrix;
   }
 }
 
 const gameOfLifeBoard = new GameBoard();
 
 console.log(gameOfLifeBoard.generateGameBoard(3, 3));
+console.log(gameOfLifeBoard.matrix);
 
 export default GameBoard;
